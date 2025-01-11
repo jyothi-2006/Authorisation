75b6bec51cbb73c2a500b872a0402f02cec51376
jyothi-2006 <gnanajyothi06@gmail.com>
2025-01-12 01:17:56 +0530

    added netlify link

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000","https://authourisation.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
